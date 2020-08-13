4ca8106f26e294f072555fa3e95ffff45ab416da
LazyMudro <69349309+LazyMudro@users.noreply.github.com>
2020-08-13 19:27:10 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,13 +6,9 @@ var instance = new vidbg(".video", {
     poster: "video/poster.jpg", // URL or relative path to fallback image
     overlay: false, // Boolean to display the overlay or not
   });
+  
   var rellax = new Rellax('.rocket');
   if (document.body.clientWidth < 600){
     rellax.destroy();
-
   }
-
-AOS.init();
-
-
- 
+  AOS.init();
